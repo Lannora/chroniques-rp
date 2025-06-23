68ddda734f9740317bb0bf3a2dba8769d7145c37
Lannora <79697076+Lannora@users.noreply.github.com>
2025-06-23 14:59:34 +0200

    Configure Next.js image settings to support remote patterns and SVG formats

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        port: "",
+        pathname: "/avatars/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        port: "",
+        pathname: "/embed/avatars/**",
+      },
+      {
+
+        protocol: "https",
+        hostname: "slqruxkrrgdrqbnzdmlw.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/characters-images/**",
+      }
+    ],
+    dangerouslyAllowSVG: true,
+      formats: ["image/avif", "image/webp"],
+  },
+}
 
 export default nextConfig;
