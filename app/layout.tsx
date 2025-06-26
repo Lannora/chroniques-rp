3c2f291df98fb2a54203f77b3292ac62ffbfe93b
Lannora <79697076+Lannora@users.noreply.github.com>
2025-06-26 19:19:10 +0200

    Add favicon and web app manifest for improved branding and mobile support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,25 @@
+// Dans app/layout.tsx
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import UserMenu from "./components/UserMenu";
+import UserMenu from "@/app/components/UserMenu";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Chronique RP",
   description: "Gérez vos personnages et vos histoires de RP.",
+  manifest: "site.webmanifest",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
 };
 
 export default async function RootLayout({
@@ -17,9 +29,11 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-gray-900 text-white`}>
         <UserMenu />
-        {children}
+        <main className="p-8 pt-24">
+          {children}
+        </main>
       </body>
     </html>
   );
